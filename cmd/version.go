1faec9a22ece779ec8c4d8adda225074980311f4
Christian Budde <Christian.Budde@meko.de>
2025-06-26 23:50:20 +0200

    fix: added release please

--- cmd/version.go
+++ cmd/version.go
@@ -6,10 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Version string = "dev"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version info",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("mail-forwarder version 0.1.0")
+		fmt.Printf("mail-reflector version %s\n", Version)
 	},
 }
