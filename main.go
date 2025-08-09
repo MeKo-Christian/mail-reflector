fdd8513d8ca3cfaf9f35de46b91fc52edf993948
Christian Budde <144226859+MeKo-Christian@users.noreply.github.com>
2025-08-09 18:47:40 +0200

    fix: only log one time (#15)

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"log/slog"
 	"os"
 
@@ -9,33 +8,6 @@ import (
 )
 
 func main() {
-	// Define the flag for log level
-	logLevel := flag.String("log-level", "info", "Set the log level (debug, info, warn, error, fatal)")
-	flag.Parse()
-
-	// Map the string value of log-level to slog.Level
-	var level slog.Level
-	switch *logLevel {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	default:
-		level = slog.LevelInfo
-	}
-
-	// Use a JSON handler for structured logs and set the log level
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	})
-
-	// Set this handler as the default for slog
-	slog.SetDefault(slog.New(handler))
-
 	// Run the command-line interface
 	if err := cmd.Execute(); err != nil {
 		slog.Error("Command execution failed", "error", err)
